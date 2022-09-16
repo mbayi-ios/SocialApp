0f0a907e9c953bee55bf053ddb4986bf10ed030c
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-16 14:32:04 +0300

    Initial Commit

---
+++ SocialApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialApp
+//
+//  Created by Amby on 16/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
